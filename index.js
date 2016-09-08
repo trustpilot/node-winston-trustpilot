720a6bc99a9eafe65c2c56fe42b992e90df6abf2
b-dur <b-dur@b-dur.dk>
2016-09-08 12:04:43 +0200

    default method only returns one transport based on NODE_ENV

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const transports = require('./transports');
 module.exports.transports = transports;
 
 module.exports.transports.default = function (options) {
-    const result = [];
     if (process.env.NODE_ENV === 'development') {
-        result.push(new transports.DevConsole(options));
+        return new transports.DevConsole(options);
     } else {
-        result.push(new transports.LogstashConsole(options));
+        return new transports.LogstashConsole(options);
     }
-    return result;
 };
 
