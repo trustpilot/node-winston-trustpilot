1d4e22bc205ae7c3ac2c7ee2ced38f5396423378
b-dur <b-dur@b-dur.dk>
2016-09-27 15:11:25 +0200

    Added an empty log function as transporter, if the NODE_ENV is 'test'

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ module.exports.transports = transports;
 module.exports.transports.default = function (options) {
     if (process.env.NODE_ENV === 'development') {
         return new transports.DevConsole(options);
+    } else if (process.env.NODE_ENV === 'test') {
+        return { log: () => {}};
     } else {
         return new transports.LogstashConsole(options);
     }
